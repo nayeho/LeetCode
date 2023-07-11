a139f50e84b6fd79d80c43b2eb88761291ef5488
Yeho Na <83909875+nayeho@users.noreply.github.com>
2023-07-12 08:23:39 +0900

    Time: 53 ms (65.35%), Space: 16.9 MB (35.18%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        graph = collections.defaultdict(list)
+        
+        def dfs(parent, child):
+            if child is None: return
+            if parent and child:
+                graph[child.val].append(parent.val)
+                graph[parent.val].append(child.val)
+            dfs(child, child.left)
+            dfs(child, child.right)
+            return
+        
+        dfs(None, root)
+        
+        queue = collections.deque([target.val])
+        visited = set()
+        visited.add(target.val)
+        while k:
+            for i in range(len(queue)):
+                parent = queue.popleft()
+                visited.add(parent)
+                for child in graph[parent]:
+                    if child not in visited:
+                        queue.append(child)
+            k -= 1
+        return list(queue)
