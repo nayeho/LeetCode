b094b6a907ed488fb2da5e7ed22d3a86516acb42
Yeho Na <83909875+nayeho@users.noreply.github.com>
2024-01-11 15:42:38 +0900

    Time: 43 ms (25.24%), Space: 17.2 MB (35.92%) - LeetHub

---
+++ 2481-minimum-cuts-to-divide-a-circle/2481-minimum-cuts-to-divide-a-circle.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfCuts(self, n: int) -> int:
+        
+        if n == 1:
+            return 0
+        elif n == 2:
+            return 1
+        elif n % 2 == 1:
+            return n
+        else:
+            return n // 2
+        
+        return -1
+        
