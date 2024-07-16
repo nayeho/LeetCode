a4a350078e70abd9bbca5a85224be164e04092cb
Yeho Na <83909875+nayeho@users.noreply.github.com>
2024-07-16 23:44:40 +0900

    Time: 239 ms (92.11%), Space: 52.1 MB (60.07%) - LeetHub

---
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getDirections(self, root: Optional[TreeNode], startValue: int, destValue: int) -> str:
+        def dfs(root: Optional[TreeNode], val: int, path: List[chr]) -> bool:
+            if root.val == val:
+                return True
+            if root.left and dfs(root.left, val, path):
+                path.append('L')
+            elif root.right and dfs(root.right, val, path):
+                path.append('R')
+            return len(path) > 0
+
+        pathToStart = []
+        pathToDest = []
+
+        dfs(root, startValue, pathToStart)
+        dfs(root, destValue, pathToDest)
+
+        while pathToStart and pathToDest and pathToStart[-1] == pathToDest[-1]:
+            pathToStart.pop()
+            pathToDest.pop()
+
+        return 'U' * len(pathToStart) + ''.join(reversed(pathToDest))
