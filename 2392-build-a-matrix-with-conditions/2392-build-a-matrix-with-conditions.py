f1b7c4f578b7a03055ea51810e8a159869da749b
Yeho Na <83909875+nayeho@users.noreply.github.com>
2024-07-21 17:56:04 +0900

    Time: 585 ms (54.20%), Space: 25.4 MB (69.47%) - LeetHub

---
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -0,0 +1,43 @@
+#review
+class Solution:
+    def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
+        rowOrder = self._topologicalSort(rowConditions, k)
+        if not rowOrder:
+            return []
+
+        colOrder = self._topologicalSort(colConditions, k)
+        if not colOrder:
+            return []
+
+        result = [[0] * k for _ in range(k)]
+        nodeToRowIndex = [0] * (k + 1)
+
+        for i, node in enumerate(rowOrder):
+            nodeToRowIndex[node] = i
+
+        for j, node in enumerate(colOrder):
+            i = nodeToRowIndex[node]
+            result[i][j] = node
+
+        return result
+
+    def _topologicalSort(self, conditions: List[List[int]], n: int) -> List[int]:
+        order = []
+        graph = [[] for _ in range(n + 1)]
+        inDegrees = [0] * (n + 1)
+
+        for u, v in conditions:
+            graph[u].append(v)
+            inDegrees[v] += 1
+
+        q = collections.deque([i for i in range(1, n + 1) if inDegrees[i] == 0])
+
+        while q:
+            u = q.popleft()
+            order.append(u)
+            for v in graph[u]:
+                inDegrees[v] -= 1
+                if inDegrees[v] == 0:
+                    q.append(v)
+
+        return order if len(order) == n else []
