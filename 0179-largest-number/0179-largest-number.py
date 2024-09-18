74ecb9990c9543c77b087a086e72adf585b544f8
Yeho Na <83909875+nayeho@users.noreply.github.com>
2024-09-18 14:23:04 +0900

    Time: 30 ms (97.29%), Space: 16.6 MB (32.99%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        return ''.join(sorted(map(str, nums), key=LargerStrKey)).lstrip('0') or '0'
+
+class LargerStrKey(str):
+    def __lt__(x: str, y: str) -> bool:
+        return x + y > y + x
