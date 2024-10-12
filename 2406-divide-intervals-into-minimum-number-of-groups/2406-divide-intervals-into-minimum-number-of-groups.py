52482b7d02e51d2242234ef66a468aaa4b10f109
Yeho Na <83909875+nayeho@users.noreply.github.com>
2024-10-12 11:11:19 +0900

    Time: 1110 ms (56.33%), Space: 55.8 MB (72.15%) - LeetHub

---
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minGroups(self, intervals: List[List[int]]) -> int:
+        minHeap = []
+
+        for left, right in sorted(intervals):
+            if minHeap and left > minHeap[0]:
+                heapq.heappop(minHeap)
+            heapq.heappush(minHeap, right)
+
+        return len(minHeap)
+    
