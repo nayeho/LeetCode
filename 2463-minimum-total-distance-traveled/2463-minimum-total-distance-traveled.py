29350b43b2bfe2f55c0f658092fa9adb52c2604a
Yeho Na <83909875+nayeho@users.noreply.github.com>
2024-10-31 20:50:40 +0900

    Time: 2012 ms (48.00%), Space: 447.9 MB (10.88%) - LeetHub

---
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
+        
+        robot.sort()
+        factory.sort()
+
+        @functools.lru_cache(None)
+        def dp(i: int, j: int, k: int) -> int:
+            if i == len(robot):
+                return 0
+            if j == len(factory):
+                return math.inf
+            skipFactory = dp(i, j + 1, 0)
+            position, limit = factory[j]
+            useFactory = (dp(i + 1, j, k + 1) + abs(robot[i] - position) if limit > k else math.inf)
+            return min(skipFactory, useFactory)
+
+        return dp(0, 0, 0)
