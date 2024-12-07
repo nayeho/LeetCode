4d8d688a4d9e0058d9a9828da2cb9ca4a37dc3d1
Yeho Na <83909875+nayeho@users.noreply.github.com>
2024-12-07 10:44:31 +0900

    Time: 603 ms (68.95%), Space: 29.4 MB (19.09%) - LeetHub

---
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
@@ -0,0 +1,5 @@
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        def numOperations(m: int) -> int:
+            return sum((num - 1) // m for num in nums) <= maxOperations
+        return bisect.bisect_left(range(1, max(nums)), True, key=lambda m: numOperations(m)) + 1
